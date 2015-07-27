62cca8c339a5fc4e504f76b29bf10f02cec656be
furti <daniel.furtlehner@gmx.net>
2015-07-27 20:12:20 +0200

    combine and minify sources to command.js

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+gulp.task('combine', function() {
+  gulp.src('build/**/*.js')
+    .pipe(concat('command.js'))
+    .pipe(gulp.dest('.'))
+    .pipe(uglify())
+    .pipe(rename({
+      extname: '.min.js'
+    }))
+    .pipe(gulp.dest('.'));
+});
