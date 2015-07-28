86eccb3c33bbdc03b3f12acbddb8150738bd5d35
furti <daniel.furtlehner@gmx.net>
2015-07-28 19:40:04 +0200

    gulp-plumber added for error handling in watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
 
 gulp.task('combine', function() {
   gulp.src('build/**/*.js')
+    .pipe(plumber())
     .pipe(concat('command.js'))
     .pipe(gulp.dest('.'))
     .pipe(uglify())
