035b0f3a0350b47682cf9fd1c6f4cba101a12f67
furti <daniel.furtlehner@gmx.net>
2015-08-05 19:46:16 +0200

    browser bundle with execute method

--- command.js
+++ command.js
@@ -38,11 +38,98 @@ var CommandJS;
         CommandExecutorImpl.prototype.getCommands = function () {
             return this.commands;
         };
+        CommandExecutorImpl.prototype.execute = function (commandString) {
+            var parsed = this.parse(commandString);
+            var commandParts = this.getCommandParts(parsed);
+            if (!commandParts) {
+                return {
+                    state: states.ExecutorResponseState.ERROR,
+                    errorType: states.ExecutorErrorType.PARSER_ERROR,
+                    commandString: commandString
+                };
+            }
+            var commandResponse = this.findCommand(commandParts, true);
+            if (commandResponse.state === states.ExecutorResponseState.ERROR) {
+                commandResponse.commandString = commandString;
+                return commandResponse;
+            }
+            try {
+                return commandResponse.response;
+            }
+            catch (e) {
+                return {
+                    state: states.ExecutorResponseState.ERROR,
+                    errorType: states.ExecutorErrorType.COMMAND_EXECUTION_ERROR,
+                    commandString: commandString,
+                    response: e
+                };
+            }
+        };
         CommandExecutorImpl.prototype.getCommand = function (commandString) {
-            var commandParts = this.getCommandParts(commandParser.parse(commandString));
+            var commandParts = this.getCommandParts(this.parse(commandString));
+            if (!commandParts) {
+                return {
+                    state: states.ExecutorResponseState.ERROR,
+                    errorType: states.ExecutorErrorType.PARSER_ERROR,
+                    commandString: commandString
+                };
+            }
+            var commandResponse = this.findCommand(commandParts, false);
+            commandResponse.commandString = commandString;
+            return commandResponse;
+        };
+        CommandExecutorImpl.prototype.findCommand = function (commandParts, paramsAllowed) {
+            if (!commandParts) {
+                return null;
+            }
+            var actualCommand;
+            var current = this.commands;
+            var i;
+            var part;
+            for (i in commandParts) {
+                part = commandParts[i];
+                if (current[part]) {
+                    actualCommand = current[part];
+                    if (actualCommand.subCommands) {
+                        current = actualCommand.subCommands;
+                    }
+                    else if (!paramsAllowed && i < commandParts.length - 1) {
+                        return {
+                            state: states.ExecutorResponseState.ERROR,
+                            errorType: states.ExecutorErrorType.COMMAND_NOT_FOUND
+                        };
+                    }
+                }
+                else {
+                    return {
+                        state: states.ExecutorResponseState.ERROR,
+                        errorType: states.ExecutorErrorType.COMMAND_NOT_FOUND,
+                        response: Object.keys(actualCommand.subCommands)
+                    };
+                }
+            }
             return {
-                state: states.ExecutorResponseState.SUCCESS
+                state: states.ExecutorResponseState.SUCCESS,
+                response: actualCommand.command
             };
+        };
+        CommandExecutorImpl.prototype.parse = function (commandString) {
+            if (!commandString) {
+                return [];
+            }
+            return commandParser.parse(commandString.trim());
+        };
+        CommandExecutorImpl.prototype.getCommandParts = function (parseResults) {
+            if (!parseResults || parseResults.length === 0) {
+                return null;
+            }
+            for (var _i = 0; _i < parseResults.length; _i++) {
+                var result = parseResults[_i];
+                if (result.type === 'COMMAND_PARAM') {
+                    return result.values;
+                }
+            }
+            return null;
         };
         CommandExecutorImpl.prototype.prepareCommands = function (commands) {
             if (!commands) {
@@ -497,6 +584,8 @@ var CommandJS;
     var ExecutorResponseState = CommandJS.ExecutorResponseState;
     (function (ExecutorErrorType) {
         ExecutorErrorType[ExecutorErrorType["COMMAND_NOT_FOUND"] = 0] = "COMMAND_NOT_FOUND";
+        ExecutorErrorType[ExecutorErrorType["PARSER_ERROR"] = 1] = "PARSER_ERROR";
+        ExecutorErrorType[ExecutorErrorType["COMMAND_EXECUTION_ERROR"] = 2] = "COMMAND_EXECUTION_ERROR";
     })(CommandJS.ExecutorErrorType || (CommandJS.ExecutorErrorType = {}));
     var ExecutorErrorType = CommandJS.ExecutorErrorType;
     module.exports = {
